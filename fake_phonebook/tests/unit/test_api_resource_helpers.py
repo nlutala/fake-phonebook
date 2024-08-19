97f0d1d9883ec5291059ffe254a99d60870c687e
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-08-19 15:54:45 +0100

    Update test_api_resource_helpers.py
    Add tests for post methods (used to add new entries of people in the phonebook)

--- fake_phonebook/tests/unit/test_api_resource_helpers.py
+++ fake_phonebook/tests/unit/test_api_resource_helpers.py
@@ -1,8 +1,16 @@
 import os
 import sqlite3
 from random import choice
+from unittest.mock import Mock
 
-from api.resources.helpers.list_people import get_people, get_person_by_id
+from api.resources.helpers.list_people import (
+    add_people_to_db,
+    get_people,
+    get_person_by_id,
+    post_person_to_db,
+)
+from faker import Faker
+from pytest_mock import mocker
 
 
 def test_get_people():
@@ -10,7 +18,6 @@ def test_get_people():
     api.resources.helpers.list_people.get_people() should return a list of dictionaries
     to represent people as id, full_name and phone_number.
     """
-
     list_of_people = get_people()
     assert str(type(list_of_people)) == "<class 'list'>"
 
@@ -34,6 +41,12 @@ def test_get_people():
 
 
 def test_get_person_by_id():
+    """
+    api.resources.helpers.list_people.get_person_by_id() should return a dictionary
+    to represent a person as id, full_name and phone_number.
+
+    If the person doesn't exist. Return null.
+    """
     # Get a list of valid ids of people in the database
     parent_dir = os.path.dirname(__file__).partition("tests")[0]
     path_to_db = os.path.join(parent_dir, "fake_people.db")
@@ -50,3 +63,107 @@ def test_get_person_by_id():
 
     # For an id that doesn't exist, this should return None
     assert get_person_by_id("this-id-doesnt-exist") is None
+
+
+def test_add_people_to_db():
+    """
+    api.resources.helpers.list_people.add_people_to_db() should return how many people
+    were inserted into the db.
+
+    It should not allow for inserting a person into the database if a record with the
+    same full_name already exists in the db
+    """
+    person1_name = Faker().name()
+    person2_name = Faker().name()
+
+    people = [
+        (
+            f"test-add-people-to-db-{choice([range(10000)])}",
+            person1_name,
+            person1_name.split(" ")[0],
+            person1_name.split(" ")[1],
+            f"""{
+                person1_name.split(" ")[0]
+            }.{
+                person1_name.split(" ")[1]
+            }@example.com""".lower(),
+            "+44 123456789",
+            f"""www.linkedin.com/{
+                person1_name.split(" ")[0]
+            }-{
+                person1_name.split(" ")[1]
+            }""".lower(),
+        ),  # Doesn't exist in the db... yet
+        (
+            f"test-add-people-to-db-{choice(range(10000))}",
+            person2_name,
+            person2_name.split(" ")[0],
+            person2_name.split(" ")[1],
+            f"""{
+                person2_name.split(" ")[0]
+            }.{
+                person2_name.split(" ")[1]
+            }@example.com""".lower(),
+            "+44 123456789",
+            f"""www.linkedin.com/{
+                person2_name.split(" ")[0]
+            }-{
+                person2_name.split(" ")[1]
+            }""".lower(),
+        ),  # Doesn't exist in the db... yet
+    ]
+
+    # Use case 1: Assert that after inserting someone into the db, add_people_to_db
+    # returns 1
+    assert add_people_to_db([people[0]]) == 1
+
+    # Use case 2: Assert that after trying to insert someone (that is already in the db)
+    # into the db, add_people_to_db returns 0
+    assert add_people_to_db([people[0]]) == 0
+
+    # Use case 3: Assert that after trying to insert someone (that is already in the db)
+    # into the db, and someone that is not into the db, add_people_to_db returns 1
+    assert add_people_to_db(people) == 1
+
+
+def test_post_person_to_db(mocker: Mock):
+    """
+    api.resources.helpers.list_people.post_person_to_db() should return the id of the
+    new person added to the database or None if the person is already in the db.
+    """
+    full_name = f"Test {Faker().name()}"
+    first_name = full_name.split(" ")[0]
+    last_name = " ".join(full_name.split(" ")[1:])
+    phone_number = "+44 123456789"
+
+    # We want to enfore that the json (well, now dictionary) representing the person
+    # Should have a full_name and phone_number key, value pairs.
+    person = {"email_address": f"{first_name}.{last_name}@example.com".lower()}
+    assert post_person_to_db(person) is None
+
+    # After adding the full_name to the person, post_person_to_db should be None
+    person["full_name"] = full_name
+    assert post_person_to_db(person) is None
+
+    mocker.patch(
+        "api.resources.helpers.list_people.add_people_to_db",
+        return_value=1,
+    )
+
+    # Now that we have added the phone number (and that this person doesn't exist in the
+    # db), post_person_to_db should return a string id for this person.
+    person["phone_number"] = phone_number
+
+    person_id = post_person_to_db(person)
+    assert person_id is not None
+    assert str(type(person_id)) == "<class 'str'>"
+
+    # Now if we try to post this person again, post_person_to_db should return None,
+    # because they now exist in the db
+    mocker.patch(
+        "api.resources.helpers.list_people.add_people_to_db",
+        return_value=0,
+    )
+
+    person_id = post_person_to_db(person)
+    assert person_id is None
