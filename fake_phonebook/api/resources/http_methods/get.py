16541873d90da721a3ac9a756d2a183b1f86463a
Nathan Lutala <87072306+nlutala@users.noreply.github.com>
2024-09-11 09:57:53 +0100

    Update http methods with sqlite3 context manager

--- fake_phonebook/api/resources/http_methods/get.py
+++ fake_phonebook/api/resources/http_methods/get.py
@@ -8,19 +8,18 @@ def get_contacts() -> list[dict[str, str]] | None:
     Returns a list of contacts and their phone number in the phonebook ordered in
     alphabetical order (a-z)
     """
-    con = sqlite3.connect(PATH_TO_DB)
-    cur = con.cursor()
-    contacts = [
-        {
-            "id": contact[0],
-            "name": contact[1],
-            "phone_number": contact[2],
-        }
-        for contact in cur.execute(
-            "SELECT id, name, phone_number FROM contacts order by name"
-        )
-    ]
-    con.close()
+    with sqlite3.connect(PATH_TO_DB) as con:
+        cur = con.cursor()
+        contacts = [
+            {
+                "id": contact[0],
+                "name": contact[1],
+                "phone_number": contact[2],
+            }
+            for contact in cur.execute(
+                "SELECT id, name, phone_number FROM contacts order by name"
+            )
+        ]
 
     return contacts if len(contacts) > 0 else None
 
@@ -34,21 +33,20 @@ def get_contact_by_id(contact_id: str) -> dict[str, str] | None:
 
     If a contact is not associated with this id, the function returns None.
     """
-    con = sqlite3.connect(PATH_TO_DB)
-    cur = con.cursor()
-    contacts = [
-        {
-            "id": contact[0],
-            "name": contact[1],
-            "phone_number": contact[2],
-        }
-        for contact in cur.execute(
-            f"""
-            SELECT id, name, phone_number FROM contacts WHERE id = '{contact_id}'
-            """
-        )
-    ]
-    con.close()
+    with sqlite3.connect(PATH_TO_DB) as con:
+        cur = con.cursor()
+        contacts = [
+            {
+                "id": contact[0],
+                "name": contact[1],
+                "phone_number": contact[2],
+            }
+            for contact in cur.execute(
+                f"""
+                SELECT id, name, phone_number FROM contacts WHERE id = '{contact_id}'
+                """
+            )
+        ]
 
     return contacts[0] if len(contacts) == 1 else None
 
@@ -66,24 +64,23 @@ def get_contacts_starting_with(string: str) -> list[dict[str, str]] | None:
     if len(string) < 1:
         return None
 
-    con = sqlite3.connect(PATH_TO_DB)
-    cur = con.cursor()
-    contacts = [
-        {
-            "id": contact[0],
-            "name": contact[1],
-            "phone_number": contact[2],
-        }
-        for contact in cur.execute(
-            f"""
-            SELECT id, name, phone_number FROM contacts
-            WHERE name LIKE '{string}%'
-            order by name
-            """.strip().replace(
-                "\n", " "
+    with sqlite3.connect(PATH_TO_DB) as con:
+        cur = con.cursor()
+        contacts = [
+            {
+                "id": contact[0],
+                "name": contact[1],
+                "phone_number": contact[2],
+            }
+            for contact in cur.execute(
+                f"""
+                SELECT id, name, phone_number FROM contacts
+                WHERE name LIKE '{string}%'
+                order by name
+                """.strip().replace(
+                    "\n", " "
+                )
             )
-        )
-    ]
-    con.close()
+        ]
 
     return contacts if len(contacts) != 0 else None
